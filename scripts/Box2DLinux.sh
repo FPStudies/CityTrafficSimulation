ee5701babf28a750e6581c6eeb114290e06574c8
Filipterra <karambatube1333@gmail.com>
2020-05-20 17:26:39 +0200

    premake5 fix (Linux)

--- scripts/Box2DLinux.sh
+++ scripts/Box2DLinux.sh
@@ -11,8 +11,8 @@ rm master.zip
 mv box2d-5ae818e95ddd09622bad4fd295311ca4706ad2b2 box2d-master
 cd box2d-master
 wget -O premake-5.0.0-alpha15-linux.tar.gz https://github.com/premake/premake-core/releases/download/v5.0.0-alpha15/premake-5.0.0-alpha15-linux.tar.gz
-unzip -q premake-5.0.0-alpha15-linux.tar.gz
+tar -zxvf premake-5.0.0-alpha15-linux.tar.gz
 rm premake-5.0.0-alpha15-linux.tar.gz
-premake5 gmake
+./premake5 gmake
 sed -i 's/PROJECTS := Box2D HelloWorld Testbed/PROJECTS := Box2D HelloWorld/g' Build/Makefile
 make -C Build
